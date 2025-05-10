65f55a20eed8bcd336235ddfd09214568d026522
jbangel7 <jbangel@unal.edu.co>
2025-05-10 09:43:34 -0500

    test internals 1

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,12 @@
+# Ejemplo del caso de uso:
+# python3 -m homework --input data/input --output data/output
 
+import sys
+
+from homework.src._internals.parse_args import parse_args
+from homework.src._internals.read_all_lines import read_all_lines
+
+
+def main():
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
